5dfcb3ddb9aff6d9e4dfd76dec1561a9b235b930
metherul <greenmarleyethan@gmail.com>
2016-09-17 01:19:11 -0400

    Data structure now works!
    Signed-off-by: metherul <greenmarleyethan@gmail.com>

---
+++ CookieClickerCounter_CCC/MainProgram.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CookieClickerCounter_CCC
+{
+    class MainProgram
+    {
+        string metaLocation = AppDomain.CurrentDomain.BaseDirectory;
+        string databaseLocation = AppDomain.CurrentDomain.BaseDirectory + "database.json";
+
+        static void Main(string[] args)
+        {
+            MainProgram program = new MainProgram();
+            program.Start();
+
+        }
+
+        public void Start()
+        {
+            //SetupDatabase();
+            UpdateDatabase("866980", "9500", "20", "2139013290901");
+        }
+
+        public void SetupDatabase()
+        {
+            List<User> userList = new List<User>();
+            User user = new User();
+            bool isRunning = true;
+
+            user = new User();
+            user.Initialize("Ethan Green", "866980");
+
+            userList.Add(user);
+
+            string jsonString = JsonConvert.SerializeObject(userList, Formatting.Indented);
+
+            File.WriteAllText(databaseLocation, jsonString);
+        }
+
+        public void UpdateDatabase(string _studentID, string _cookieCount, string _cookiesPerSecond, string _saveString)
+        {
+            int counter = 0;
+
+            string jsonData = File.ReadAllText(databaseLocation);
+            string studentName;
+            string studentID = _studentID;
+            string cookieCount = _cookieCount;
+            string cookiesPerSecond = _cookiesPerSecond;
+            string saveString = _saveString;
+
+            bool isRunning = true;
+
+            dynamic jsonDocument = JsonConvert.DeserializeObject<List<User>>(jsonData);
+
+            while (isRunning)
+            {
+                Console.WriteLine(counter);
+
+                if ((jsonDocument[counter].StudentID) == studentID)
+                    isRunning = false;
+
+                else
+                    counter++;
+            }
+
+            studentName = jsonDocument[counter].Name;
+
+            // Oh god
+            // Cookie count
+
+            if (jsonDocument[counter].CookieCount != null)
+            {
+                jsonDocument[counter].CookieCountArchive.Add(jsonDocument[counter].CookieCount);
+                jsonDocument[counter].CookieCount = cookieCount;
+            }
+
+            else
+                jsonDocument[counter].CookieCount = cookieCount;
+
+            // Cookies per second
+
+            if (jsonDocument[counter].CookiesPerSecond != null)
+            {
+                jsonDocument[counter].CookiesPerSecondArchive.Add(jsonDocument[counter].CookieCount);
+                jsonDocument[counter].CookiesPerSecond = cookiesPerSecond;
+            }
+
+            else
+                jsonDocument[counter].CookiesPerSecond = cookiesPerSecond;
+
+            // SaveString
+
+            if (jsonDocument[counter].SaveString != null)
+            {
+                jsonDocument[counter].SaveStringArchive.Add(jsonDocument[counter].SaveString);
+                jsonDocument[counter].SaveString = saveString;
+            }
+
+            else
+                jsonDocument[counter].SaveString = saveString;
+
+            // TimeStamp
+
+            if (jsonDocument[counter].TimeStamp != null)
+            {
+                jsonDocument[counter].TimeStampArchive.Add(jsonDocument[counter].TimeStamp);
+                jsonDocument[counter].TimeStamp = DateTime.Now.ToString();
+            }
+
+            else
+                jsonDocument[counter].TimeStamp = DateTime.Now.ToString();
+
+            string jsonString = JsonConvert.SerializeObject(jsonDocument, Formatting.Indented);
+
+            File.WriteAllText(databaseLocation, jsonString);
+
+        }
+
+        public void ClearDatabase()
+        {
+            bool run = false;
+
+            if (run)
+            {
+                File.Delete(databaseLocation);
+            }
+        }
+    }
+}
---
+++ CookieClickerCounter_CCC/User.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClickerCounter_CCC
+{
+    [JsonObject(Title = "User")]
+    public class User
+    {
+        public string Name { get; set; }
+        public string StudentID { get; set; }
+        public string CookieCount { get; set; }
+        public string CookiesPerSecond { get; set; }
+        public string SaveString { get; set; }
+        public string TimeStamp { get; set; }
+        public List<object> CookieCountArchive { get; set; }
+        public List<object> CookiesPerSecondArchive { get; set; }
+        public List<object> SaveStringArchive { get; set; }
+        public List<object> TimeStampArchive { get; set; }
+
+        public User()
+        {
+            CookieCountArchive = new List<object>();
+            CookiesPerSecondArchive = new List<object>();
+            SaveStringArchive = new List<object>();
+            TimeStampArchive = new List<object>();
+        }
+
+        public void Initialize(string _name, string _studentID)
+        {
+            Name = _name;
+            StudentID = _studentID;
+        }
+
+        public void Update(string _cookieCount, string _cookiesPerSecond, string _saveString)
+        {
+            if (CookieCount != string.Empty)
+                CookieCountArchive.Add(CookieCount);
+
+            if (CookiesPerSecond != string.Empty)
+                CookiesPerSecondArchive.Add(CookiesPerSecond);
+
+            if (SaveString != string.Empty)
+                SaveStringArchive.Add(SaveString);
+
+            if (TimeStamp != string.Empty)
+                TimeStampArchive.Add(TimeStamp);
+
+            CookieCount = _cookieCount;
+            CookiesPerSecond = _cookiesPerSecond;
+            SaveString = _saveString;
+            TimeStamp = DateTime.Now.ToString();
+        }
+    }
+
+    public class RootObject
+    {
+        public List<User> Users { get; set; }
+    }
+}
+
+
